f0d852deba81098eeb5f11e888ebe46405198bc6
Raymond LeClair <raymond.leclair@gmail.com>
2026-02-06 08:21:04 -0500

    Blacken

--- members/models.py
+++ members/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     """
     Custom user model for SpeakUp.
     Differentiates between Guests and official Members.
     """
+
     is_guest = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=20, blank=True)
-    
+
     # Toastmaster specific fields
     join_date = models.DateField(null=True, blank=True)
-    is_officer = models.BooleanField(default=False, help_text="Can manage meeting agendas")
-    
+    is_officer = models.BooleanField(
+        default=False, help_text="Can manage meeting agendas"
+    )
+
     # Self-referential Key for Mentorship
-    mentor = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='mentees')
+    mentor = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.SET_NULL, related_name="mentees"
+    )
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}" if self.first_name else self.username
+        return (
+            f"{self.first_name} {self.last_name}" if self.first_name else self.username
+        )
