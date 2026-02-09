3beb1196122e0c226bf42c4f6d5d0a34168929b3
Raymond LeClair <raymond.leclair@gmail.com>
2026-02-09 13:41:48 -0500

    Add button to announcement change form

--- communications/admin.py
+++ communications/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils import timezone
-from django.contrib import messages
+
 from .models import Announcement
 
 
@@ -9,6 +9,7 @@ class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ("subject", "audience", "created_at", "sent_at")
     readonly_fields = ("sent_at",)
     actions = ["send_announcement"]
+    change_form_template = "communications/admin/announcement_change_form.html"
 
     @admin.action(description="Send selected announcements via Email")
     def send_announcement(self, request, queryset):
@@ -21,3 +22,17 @@ class AnnouncementAdmin(admin.ModelAdmin):
                 f"Sent '{announcement.subject}' to {count} recipients.",
                 messages.SUCCESS,
             )
+
+    def response_change(self, request, obj):
+        """Handle the 'Send Announcement' button on the change form."""
+        if "_send-announcement" in request.POST:
+            count = obj.send()
+            obj.sent_at = timezone.now()
+            obj.save()
+            self.message_user(
+                request,
+                f"Sent '{obj.subject}' to {count} recipients.",
+                messages.SUCCESS,
+            )
+            return self.response_post_save_change(request, obj)
+        return super().response_change(request, obj)
