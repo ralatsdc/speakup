9c26a10887bf404f45e5eda802543cbf7aa38a10
Raymond LeClair <raymond.leclair@gmail.com>
2026-02-14 10:46:28 -0500

    Remove unneeded scripts

--- postgres/dump.sh
+++
@@ -1,4 +0,0 @@
-# https://www.postgresql.org/docs/17/app-pgdump.html
-. .zshenv
-echo "password: $PGPASSWORD"
-pg_dump -U $PGUSER -h $PGHOST -p $PGPORT -W -F t $PGDATABASE > dump-$(date "+%Y-%m-%dT%H:%M:%S").tar
